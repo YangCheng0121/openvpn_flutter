21f5a66913a31fb6052463001cb15071999d4095
yangcheng <yang759126596@163.com>
2024-11-01 09:34:28 +0800

    Update SwiftOpenVPNFlutterPlugin.swift

--- ios/Classes/SwiftOpenVPNFlutterPlugin.swift
+++ ios/Classes/SwiftOpenVPNFlutterPlugin.swift
@@ -3,104 +3,101 @@ import UIKit
 import NetworkExtension
 
 public class SwiftOpenVPNFlutterPlugin: NSObject, FlutterPlugin {
-    private static var utils : VPNUtils! = VPNUtils()
+    private static var utils: VPNUtils! = VPNUtils()
     
     private static var EVENT_CHANNEL_VPN_STAGE = "id.laskarmedia.openvpn_flutter/vpnstage"
     private static var METHOD_CHANNEL_VPN_CONTROL = "id.laskarmedia.openvpn_flutter/vpncontrol"
-     
+    
     public static var stage: FlutterEventSink?
-    private var initialized : Bool = false
+    private var initialized: Bool = false
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = SwiftOpenVPNFlutterPlugin()
         instance.onRegister(registrar)
     }
     
-    public func onRegister(_ registrar: FlutterPluginRegistrar){
+    public func onRegister(_ registrar: FlutterPluginRegistrar) {
+        // 创建方法通道和事件通道
         let vpnControlM = FlutterMethodChannel(name: SwiftOpenVPNFlutterPlugin.METHOD_CHANNEL_VPN_CONTROL, binaryMessenger: registrar.messenger())
         let vpnStageE = FlutterEventChannel(name: SwiftOpenVPNFlutterPlugin.EVENT_CHANNEL_VPN_STAGE, binaryMessenger: registrar.messenger())
         
         vpnStageE.setStreamHandler(StageHandler())
-        vpnControlM.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        vpnControlM.setMethodCallHandler({ (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
             switch call.method {
             case "status":
                 SwiftOpenVPNFlutterPlugin.utils.getTraffictStats()
                 result(UserDefaults.init(suiteName: SwiftOpenVPNFlutterPlugin.utils.groupIdentifier)?.string(forKey: "connectionUpdate"))
-                break;
+                break
             case "stage":
                 result(SwiftOpenVPNFlutterPlugin.utils.currentStatus())
-                break;
+                break
             case "initialize":
+                // 解析初始化参数
                 let providerBundleIdentifier: String? = (call.arguments as? [String: Any])?["providerBundleIdentifier"] as? String
                 let localizedDescription: String? = (call.arguments as? [String: Any])?["localizedDescription"] as? String
                 let groupIdentifier: String? = (call.arguments as? [String: Any])?["groupIdentifier"] as? String
-                if providerBundleIdentifier == nil  {
-                    result(FlutterError(code: "-2",
-                                        message: "providerBundleIdentifier content empty or null",
-                                        details: nil));
-                    return;
+                
+                // 检查参数是否为空
+                if providerBundleIdentifier == nil {
+                    result(FlutterError(code: "-2", message: "providerBundleIdentifier 内容为空或为 null", details: nil))
+                    return
                 }
-                if localizedDescription == nil  {
-                    result(FlutterError(code: "-3",
-                                        message: "localizedDescription content empty or null",
-                                        details: nil));
-                    return;
+                if localizedDescription == nil {
+                    result(FlutterError(code: "-3", message: "localizedDescription 内容为空或为 null", details: nil))
+                    return
                 }
-                if groupIdentifier == nil  {
-                    result(FlutterError(code: "-4",
-                                        message: "groupIdentifier content empty or null",
-                                        details: nil));
-                    return;
-                }
-                SwiftOpenVPNFlutterPlugin.utils.groupIdentifier = groupIdentifier
-                SwiftOpenVPNFlutterPlugin.utils.localizedDescription = localizedDescription
-                SwiftOpenVPNFlutterPlugin.utils.providerBundleIdentifier = providerBundleIdentifier
-                SwiftOpenVPNFlutterPlugin.utils.loadProviderManager{(err:Error?) in
-                    if err == nil{
-                        result(SwiftOpenVPNFlutterPlugin.utils.currentStatus())
-                    }else{
-                        result(FlutterError(code: "-4", message: err?.localizedDescription, details: err?.localizedDescription));
-                    }
-                }
-                self.initialized = true
-                break;
-            case "disconnect":
-                SwiftOpenVPNFlutterPlugin.utils.stopVPN()
-                break;
-            case "connect":
-                if !self.initialized {
-                    result(FlutterError(code: "-1",
-                                        message: "VPNEngine need to be initialize",
-                                        details: nil));
-                }
-                let config: String? = (call.arguments as? [String : Any])? ["config"] as? String
-                let username: String? = (call.arguments as? [String : Any])? ["username"] as? String
-                let password: String? = (call.arguments as? [String : Any])? ["password"] as? String
-                if config == nil{
-                    result(FlutterError(code: "-2",
-                                        message:"Config is empty or nulled",
-                                        details: "Config can't be nulled"))
+                if groupIdentifier == nil {
+                    result(FlutterError(code: "-4", message: "groupIdentifier 内容为空或为 null", details: nil))
                     return
                 }
                 
-                SwiftOpenVPNFlutterPlugin.utils.configureVPN(config: config, username: username, password: password, completion: {(success:Error?) -> Void in
-                    if(success == nil){
+                // 设置 VPN 配置
+                SwiftOpenVPNFlutterPlugin.utils.groupIdentifier = groupIdentifier
+                SwiftOpenVPNFlutterPlugin.utils.localizedDescription = localizedDescription
+                SwiftOpenVPNFlutterPlugin.utils.providerBundleIdentifier = providerBundleIdentifier
+                
+                SwiftOpenVPNFlutterPlugin.utils.loadProviderManager { (err: Error?) in
+                    if err == nil {
+                        result(SwiftOpenVPNFlutterPlugin.utils.currentStatus())
+                    } else {
+                        result(FlutterError(code: "-4", message: err?.localizedDescription, details: err?.localizedDescription))
+                    }
+                }
+                self.initialized = true
+                break
+            case "disconnect":
+                // 断开 VPN 连接
+                SwiftOpenVPNFlutterPlugin.utils.stopVPN()
+                break
+            case "connect":
+                if !self.initialized {
+                    result(FlutterError(code: "-1", message: "VPNEngine 需要初始化", details: nil))
+                }
+                let config: String? = (call.arguments as? [String: Any])?["config"] as? String
+                let username: String? = (call.arguments as? [String: Any])?["username"] as? String
+                let password: String? = (call.arguments as? [String: Any])?["password"] as? String
+                
+                if config == nil {
+                    result(FlutterError(code: "-2", message: "Config 为空或为 null", details: "Config 不能为空"))
+                    return
+                }
+                
+                // 配置并连接 VPN
+                SwiftOpenVPNFlutterPlugin.utils.configureVPN(config: config, username: username, password: password, completion: { (success: Error?) -> Void in
+                    if success == nil {
                         result(nil)
-                    }else{
-                        result(FlutterError(code: "99",
-                                            message: "permission denied",
-                                            details: success?.localizedDescription))
+                    } else {
+                        result(FlutterError(code: "99", message: "权限被拒绝", details: success?.localizedDescription))
                     }
                 })
-                break;
+                break
             case "dispose":
                 self.initialized = false
             default:
-                break;
+                break
             }
         })
     }
-    
     
     class StageHandler: NSObject, FlutterStreamHandler {
         func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
@@ -113,22 +110,20 @@ public class SwiftOpenVPNFlutterPlugin: NSObject, FlutterPlugin {
             return nil
         }
     }
-    
-    
 }
-
 
 @available(iOS 9.0, *)
 class VPNUtils {
     var providerManager: NETunnelProviderManager!
-    var providerBundleIdentifier : String?
-    var localizedDescription : String?
-    var groupIdentifier : String?
-    var stage : FlutterEventSink!
-    var vpnStageObserver : NSObjectProtocol?
+    var providerBundleIdentifier: String?
+    var localizedDescription: String?
+    var groupIdentifier: String?
+    var stage: FlutterEventSink!
+    var vpnStageObserver: NSObjectProtocol?
     
-    func loadProviderManager(completion:@escaping (_ error : Error?) -> Void)  {
-        NETunnelProviderManager.loadAllFromPreferences { (managers, error)  in
+    func loadProviderManager(completion: @escaping (_ error: Error?) -> Void) {
+        // 从偏好设置中加载 VPN 提供程序管理器
+        NETunnelProviderManager.loadAllFromPreferences { (managers, error) in
             if error == nil {
                 self.providerManager = managers?.first ?? NETunnelProviderManager()
                 completion(nil)
@@ -138,63 +133,67 @@ class VPNUtils {
         }
     }
     
-    func onVpnStatusChanged(notification : NEVPNStatus) {
+    func onVpnStatusChanged(notification: NEVPNStatus) {
+        // 处理 VPN 状态变化
         switch notification {
         case NEVPNStatus.connected:
             stage?("connected")
-            break;
+            break
         case NEVPNStatus.connecting:
             stage?("connecting")
-            break;
+            break
         case NEVPNStatus.disconnected:
             stage?("disconnected")
-            break;
+            break
         case NEVPNStatus.disconnecting:
             stage?("disconnecting")
-            break;
+            break
         case NEVPNStatus.invalid:
             stage?("invalid")
-            break;
+            break
         case NEVPNStatus.reasserting:
             stage?("reasserting")
-            break;
+            break
         default:
             stage?("null")
-            break;
+            break
         }
     }
     
-    func onVpnStatusChangedString(notification : NEVPNStatus?) -> String?{
+    func onVpnStatusChangedString(notification: NEVPNStatus?) -> String? {
+        // 将 VPN 状态转换为字符串
         if notification == nil {
             return "disconnected"
         }
         switch notification! {
         case NEVPNStatus.connected:
-            return "connected";
+            return "connected"
         case NEVPNStatus.connecting:
-            return "connecting";
+            return "connecting"
         case NEVPNStatus.disconnected:
-            return "disconnected";
+            return "disconnected"
         case NEVPNStatus.disconnecting:
-            return "disconnecting";
+            return "disconnecting"
         case NEVPNStatus.invalid:
-            return "invalid";
+            return "invalid"
         case NEVPNStatus.reasserting:
-            return "reasserting";
+            return "reasserting"
         default:
-            return "";
+            return ""
         }
     }
     
     func currentStatus() -> String? {
+        // 获取当前 VPN 状态
         if self.providerManager != nil {
-            return onVpnStatusChangedString(notification: self.providerManager.connection.status)}
-        else{
+            return onVpnStatusChangedString(notification: self.providerManager.connection.status)
+        } else {
             return "disconnected"
         }
     }
     
-    func configureVPN(config: String?, username : String?,password : String?,completion:@escaping (_ error : Error?) -> Void) {
+    func configureVPN(config: String?, username: String?, password: String?, completion: @escaping (_ error: Error?) -> Void) {
+        // 配置 VPN
         let configData = config
         self.providerManager?.loadFromPreferences { error in
             if error == nil {
@@ -205,73 +204,72 @@ class VPNUtils {
                 tunnelProtocol.providerConfiguration = [
                     "config": configData?.data(using: .utf8) ?? nullData!,
                     "groupIdentifier": self.groupIdentifier?.data(using: .utf8) ?? nullData!,
-                    "username" : username?.data(using: .utf8) ?? nullData!,
-                    "password" : password?.data(using: .utf8) ?? nullData!
+                    "username": username?.data(using: .utf8) ?? nullData!,
+                    "password": password?.data(using: .utf8) ?? nullData!
                 ]
                 tunnelProtocol.disconnectOnSleep = false
                 self.providerManager.protocolConfiguration = tunnelProtocol
-                self.providerManager.localizedDescription = self.localizedDescription // the title of the VPN profile which will appear on Settings
+                self.providerManager.localizedDescription = self.localizedDescription // VPN 配置在设置中显示的标题
                 self.providerManager.isEnabled = true
                 self.providerManager.saveToPreferences(completionHandler: { (error) in
-                    if error == nil  {
+                    if error == nil {
                         self.providerManager.loadFromPreferences(completionHandler: { (error) in
                             if error != nil {
-                                completion(error);
-                                return;
+                                completion(error)
+                                return
                             }
                             do {
+                                // 观察 VPN 状态变化
                                 if self.vpnStageObserver != nil {
                                     NotificationCenter.default.removeObserver(self.vpnStageObserver!,
                                                                               name: NSNotification.Name.NEVPNStatusDidChange,
                                                                               object: nil)
                                 }
                                 self.vpnStageObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.NEVPNStatusDidChange,
-                                                                                               object: nil ,
+                                                                                               object: nil,
                                                                                                queue: nil) { [weak self] notification in
                                     let nevpnconn = notification.object as! NEVPNConnection
                                     let status = nevpnconn.status
                                     self?.onVpnStatusChanged(notification: status)
                                 }
                                 
-                                if username != nil && password != nil{
-                                    let options: [String : NSObject] = [
+                                // 开始 VPN 连接
+                                if username != nil && password != nil {
+                                    let options: [String: NSObject] = [
                                         "username": username! as NSString,
                                         "password": password! as NSString
                                     ]
                                     try self.providerManager.connection.startVPNTunnel(options: options)
-                                }else{
-                                    try self.providerManager.connection.startVPNTunnel()
+                                    completion(nil)
+                                } else {
+                                    completion(FlutterError(code: "-2", message: "用户名或密码为空", details: nil))
                                 }
-                                completion(nil);
-                            } catch let error {
-                                self.stopVPN()
-                                print("Error info: \(error)")
-                                completion(error);
+                            } catch let e {
+                                completion(e)
                             }
                         })
                     } else {
-                        completion(error);
+                        completion(error)
                     }
                 })
+            } else {
+                completion(error)
             }
         }
-        
-        
     }
     
     func stopVPN() {
-        self.providerManager.connection.stopVPNTunnel();
+        // 停止 VPN 连接
+        if self.providerManager != nil {
+            self.providerManager.connection.stopVPNTunnel()
+            stage?("disconnected")
+        }
     }
     
-    func getTraffictStats(){
-        if let session = self.providerManager?.connection as? NETunnelProviderSession {
-            do {
-                try session.sendProviderMessage("OPENVPN_STATS".data(using: .utf8)!) {(data) in
-                    //Do nothing
-                }
-            } catch {
-            // some error
-            }
+    func getTraffictStats() {
+        // 获取流量统计信息
+        if let stats = UserDefaults(suiteName: self.groupIdentifier!)?.string(forKey: "connectionUpdate") {
+            stage?(stats)
         }
     }
 }
